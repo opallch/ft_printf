a6e56e5767262942ec0111a14be29f9393edb5e8
Opal Leung <oleung@student.42berlin.de>
2023-12-07 14:04:23 +0100

    print char

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oleung <oleung@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: oleung <oleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 08:42:05 by oleung            #+#    #+#             */
-/*   Updated: 2023/11/29 09:11:17 by oleung           ###   ########.fr       */
+/*   Updated: 2023/12/07 14:04:02 by oleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 
 int ft_printf(const char *format, ...)
 {
+    size_t  i;
+    va_list args;
     
+    i = 0;
+    va_start(args, format);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            /* 
+            check format[i + 1]
+            %
+            d or i
+            u
+            x or X
+            s
+            p
+            */
+           if (format[i + 1] == 'c')
+            ft_putchar_fd(va_arg(args, int), 0);
+           i++; 
+        }
+        else
+            ft_putchar_fd(format[i], 0);
+        i++;
+    }
+    va_end(args);
+    return (0);
 }
