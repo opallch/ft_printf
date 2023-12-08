91c45ef9e365c6a6f96cb6595d2182465b4ef4a6
Opal Leung <oleung@student.42berlin.de>
2023-12-08 16:16:31 +0100

    change fd from 0 to 1

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: oleung <oleung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 08:42:05 by oleung            #+#    #+#             */
-/*   Updated: 2023/12/07 15:13:05 by oleung           ###   ########.fr       */
+/*   Updated: 2023/12/08 16:13:44 by oleung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,24 @@ int ft_printf(const char *format, ...)
         {
            i++; 
            if (format[i] == 'c')
-            ft_putchar_fd((char) va_arg(args, int), 0);
+            ft_putchar_fd((char) va_arg(args, int), 1);
            else if (format[i] == 's')
-            ft_putstr_fd(va_arg(args, char *), 0);
+            ft_putstr_fd(va_arg(args, char *), 1);
             else if (format[i] == '%')
-                ft_putchar_fd('%', 0);
+                ft_putchar_fd('%', 1);
             else if (format[i] == 'd' || format[i] == 'i')
-                ft_putnbr_fd(va_arg(args, int), 0);
+                ft_putnbr_fd(va_arg(args, int), 1);
             else if (format[i] == 'u')
-                ft_putnbr_fd(va_arg(args, unsigned int), 0);
+                ft_putnbr_fd(va_arg(args, unsigned int), 1);
             else if (format[i] == 'x')
-                ft_putnbr_base_fd(va_arg(args, int), "0123456789abcdef", 0);
+                ft_putnbr_base_fd(va_arg(args, int), "0123456789abcdef", 1);
             else if (format[i] == 'X')
-                ft_putnbr_base_fd(va_arg(args, int), "0123456789ABCDEF", 0);
+                ft_putnbr_base_fd(va_arg(args, int), "0123456789ABCDEF", 1);
             else if (format[i] == 'p')
-                ft_putptr_fd(va_arg(args, void *), 0);
+                ft_putptr_fd(va_arg(args, void *), 1);
         }
         else
-            ft_putchar_fd(format[i], 0);
+            ft_putchar_fd(format[i], 1);
         i++;
     }
     va_end(args);
